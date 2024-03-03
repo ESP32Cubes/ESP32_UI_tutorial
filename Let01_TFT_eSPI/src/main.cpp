fa4915f4d4a579413dac669cc28a2e6535de5fa9
colin yang <yangjianlin@gmail.com>
2024-03-03 12:34:40 +0800

    update 01 code

--- Let01_TFT_eSPI/src/main.cpp
+++ Let01_TFT_eSPI/src/main.cpp
@@ -8,16 +8,18 @@ TFT_eSPI tft = TFT_eSPI(); // Invoke library
 void setup()
 {
   Serial.begin(115200);
-
+  // tft init
   tft.init();
   tft.setRotation(3);
-}
 
-void loop()
-{
+  // tft test
   tft.fillScreen(TFT_BLUE);
   tft.setCursor(50, 50, 4);
   tft.setTextColor(TFT_WHITE);
   tft.println("Hello ESP32Cube!");
   delay(2000);
+}
+
+void loop()
+{
 }
